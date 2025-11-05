a3ce4b3e68a4dae1e00de970d34f5b86274804dd
rubiliks <rubiliks@gmail.com>
2025-11-05 18:15:02 +0300

    add cam connect

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+# lib import
 import sys
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+from PySide6.QtGui import QPixmap, QImage
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 from ctypes import *
-
 from MvCameraControl_class import *
 
+#entry point
 if __name__ == "__main__":
 
-    # ch:初始化SDK | en: initialize SDK
     MvCamera.MV_CC_Initialize()
-
     deviceList = MV_CC_DEVICE_INFO_LIST()
     tlayerType = (MV_GIGE_DEVICE | MV_USB_DEVICE | MV_GENTL_CAMERALINK_DEVICE
                   | MV_GENTL_CXP_DEVICE | MV_GENTL_XOF_DEVICE)
 
-    # ch:枚举设备 | en:Enum device
+    # find all cam
     ret = MvCamera.MV_CC_EnumDevices(tlayerType, deviceList)
     if ret != 0:
         print("enum devices fail! ret[0x%x]" % ret)
@@ -28,6 +29,7 @@ if __name__ == "__main__":
 
     print ("Find %d devices!" % deviceList.nDeviceNum)
 
+    # print info for all  gige cam
     for i in range(0, deviceList.nDeviceNum):
         mvcc_dev_info = cast(deviceList.pDeviceInfo[i], POINTER(MV_CC_DEVICE_INFO)).contents
         if mvcc_dev_info.nTLayerType == MV_GIGE_DEVICE or mvcc_dev_info.nTLayerType == MV_GENTL_GIGE_DEVICE:
@@ -40,44 +42,104 @@ if __name__ == "__main__":
             nip3 = ((mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x0000ff00) >> 8)
             nip4 = (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x000000ff)
             print("current ip: %d.%d.%d.%d\n" % (nip1, nip2, nip3, nip4))
-        elif mvcc_dev_info.nTLayerType == MV_USB_DEVICE:
-            print("\nu3v device: [%d]" % i)
-            strModeName = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stUsb3VInfo.chModelName if c != 0])
-            print("device model name: %s" % strModeName)
 
-            strSerialNumber = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stUsb3VInfo.chSerialNumber if c != 0])
-            print("user serial number: %s" % strSerialNumber)
-        elif mvcc_dev_info.nTLayerType == MV_GENTL_CAMERALINK_DEVICE:
-            print("\nCML device: [%d]" % i)
-            strModeName = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stCMLInfo.chModelName if c != 0])
-            print("device model name: %s" % strModeName)
-
-            strSerialNumber = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stCMLInfo.chSerialNumber if c != 0])
-            print("user serial number: %s" % strSerialNumber)
-        elif mvcc_dev_info.nTLayerType == MV_GENTL_CXP_DEVICE:
-            print("\nCXP device: [%d]" % i)
-            strModeName = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stCXPInfo.chModelName if c != 0])
-            print("device model name: %s" % strModeName)
-
-            strSerialNumber = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stCXPInfo.chSerialNumber if c != 0])
-            print("user serial number: %s" % strSerialNumber)
-        elif mvcc_dev_info.nTLayerType == MV_GENTL_XOF_DEVICE:
-            print("\nXoF device: [%d]" % i)
-            strModeName = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stXoFInfo.chModelName if c != 0])
-            print("device model name: %s" % strModeName)
-
-            strSerialNumber = ''.join([chr(c) for c in mvcc_dev_info.SpecialInfo.stXoFInfo.chSerialNumber if c != 0])
-            print("user serial number: %s" % strSerialNumber)
-
-    nConnectionNum = input("please input the number of the device to connect:")
-
+    # connect to 0 id cam
+    nConnectionNum = 0
     if int(nConnectionNum) >= deviceList.nDeviceNum:
         print("intput error!")
         sys.exit()
 
+    cam = MvCamera()
+    stDeviceList = cast(deviceList.pDeviceInfo[int(nConnectionNum)], POINTER(MV_CC_DEVICE_INFO)).contents
+
+    # create handle
+    ret = cam.MV_CC_CreateHandle(stDeviceList)
+    if ret != 0:
+        print ("create handle fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    # open cam
+    ret = cam.MV_CC_OpenDevice(MV_ACCESS_Exclusive, 0)
+    if ret != 0:
+        print ("open device fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    # Detection network optimal package size(It only works for the GigE camera)
+    if stDeviceList.nTLayerType == MV_GIGE_DEVICE or stDeviceList.nTLayerType == MV_GENTL_GIGE_DEVICE:
+        nPacketSize = cam.MV_CC_GetOptimalPacketSize()
+        if int(nPacketSize) > 0:
+            ret = cam.MV_CC_SetIntValue("GevSCPSPacketSize",nPacketSize)
+            if ret != 0:
+                print ("Warning: Set Packet Size fail! ret[0x%x]" % ret)
+        else:
+            print ("Warning: Get Packet Size fail! ret[0x%x]" % nPacketSize)
+
+    #Set trigger mode as off
+    ret = cam.MV_CC_SetEnumValue("TriggerMode", MV_TRIGGER_MODE_OFF)
+    if ret != 0:
+        print ("set trigger mode fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    # Start grab image
+    ret = cam.MV_CC_StartGrabbing()
+    if ret != 0:
+        print ("start grabbing fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    ##################################################
+
+    stOutFrame = MV_FRAME_OUT()
+    memset(byref(stOutFrame), 0, sizeof(stOutFrame))
+    ret = cam.MV_CC_GetImageBuffer(stOutFrame, 1000)
+
+
+    if None != stOutFrame.pBufAddr and 0 == ret:
+        print("get one frame: Width[%d], Height[%d], nFrameNum[%d]" % (stOutFrame.stFrameInfo.nWidth,
+                                                                       stOutFrame.stFrameInfo.nHeight,
+                                                                       stOutFrame.stFrameInfo.nFrameNum))
+        nRet = cam.MV_CC_FreeImageBuffer(stOutFrame)
+    else:
+        print("no data[0x%x]" % ret)
+
+    # Преобразование данных в QImage
+    width = stOutFrame.stFrameInfo.nWidth
+    height = stOutFrame.stFrameInfo.nHeight
+    pixel_type = stOutFrame.stFrameInfo.enPixelType
+    print(stOutFrame.stFrameInfo.nWidth)
+    print(stOutFrame.stFrameInfo.enPixelType)
+
+    qimage = QImage(stOutFrame.pBufAddr, width, height, width * 3,QImage.Format_Grayscale8)
+    pixmap = QPixmap.fromImage(qimage)
+
+
+
+
+
+
+
+    # Stop grab image
+    ret = cam.MV_CC_StopGrabbing()
+    if ret != 0:
+        print ("stop grabbing fail! ret[0x%x]" % ret)
+        sys.exit()
+
+    # Close device
+    ret = cam.MV_CC_CloseDevice()
+    if ret != 0:
+        print ("close deivce fail! ret[0x%x]" % ret)
+        sys.exit()
+    # Destroy handle
+    ret = cam.MV_CC_DestroyHandle()
+    if ret != 0:
+        print ("destroy handle fail! ret[0x%x]" % ret)
+        sys.exit()
+
+
+
 
 
     app = QApplication(sys.argv)
+
     label = QLabel("Hello World", alignment=Qt.Alignment.AlignCenter)
     label.show()
     sys.exit(app.exec())
