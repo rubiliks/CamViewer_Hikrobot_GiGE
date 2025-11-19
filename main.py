afff3ab0bd87efe0921c783ec42dca2ef7a2d892
rubiliks <rubiliks@gmail.com>
2025-11-19 10:48:49 +0300

    Add class cnn yolo

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ class HikCam():
         self.deviceList = MV_CC_DEVICE_INFO_LIST()
         self.mem_connect = False
         self.stDeviceList = 0
+        self.ExposureTime = 1000
+        self.Gain = 2.0
 
     def _update_cam_list(self):
         ret = self.cam.MV_CC_EnumDevices(MV_GIGE_DEVICE, self.deviceList)
@@ -55,12 +57,14 @@ class HikCam():
                 nip4 = (mvcc_dev_info.SpecialInfo.stGigEInfo.nCurrentIp & 0x000000ff)
                 print("current ip: %d.%d.%d.%d\n" % (nip1, nip2, nip3, nip4))
 
-    def _create_cam_handle_open_setting(self):
+    def _create_cam_handle_open_setting_start_grab(self):
+
         # _update_cam_list
         if int(self.nConnectionNum) >= self.deviceList.nDeviceNum:
             print("intput error!")
             sys.exit()
         self.stDeviceList = cast(self.deviceList.pDeviceInfo[int(self.nConnectionNum)], POINTER(MV_CC_DEVICE_INFO)).contents
+
         # _сreate Handle
         ret = self.cam.MV_CC_CreateHandle(self.stDeviceList)
         if ret != 0:
@@ -68,6 +72,7 @@ class HikCam():
             sys.exit()
         else:
             print("created handle ")
+
         # _open camera
         ret = self.cam.MV_CC_OpenDevice(MV_ACCESS_Exclusive, 0)
         if ret != 0:
@@ -75,6 +80,137 @@ class HikCam():
             sys.exit()
         else:
             print("device open ")
+
+        # _set_camera_setting
+        print("set camera setting")
+        # Set trigger mode as off
+        ret = self.cam.MV_CC_SetEnumValue("TriggerMode", MV_TRIGGER_MODE_OFF)
+        if ret != 0:
+            print("set trigger mode fail! ret[0x%x]" % ret)
+            sys.exit()
+        else:
+            print("set trigger mode off")
+
+        # Set gain mode
+        ret = self.cam.MV_CC_SetEnumValue("GainAuto", MV_GAIN_MODE_OFF)
+        if ret != 0:
+            print("set GainAuto mode fail! ret[0x%x]" % ret)
+            sys.exit()
+        else:
+            print("set GainAuto mode off")
+
+        # Set BalanceWhiteAuto as off
+        ret = self.cam.MV_CC_SetEnumValue("BalanceWhiteAuto", MV_BALANCEWHITE_AUTO_OFF)
+        if ret != 0:
+            print("set BalanceWhiteAuto mode fail! ret[0x%x]" % ret)
+            sys.exit()
+        else:
+            print("set BalanceWhiteAuto mode off")
+
+        # Set ExposureAuto  as off
+        ret = self.cam.MV_CC_SetEnumValue("ExposureAuto", MV_EXPOSURE_AUTO_MODE_OFF)
+        if ret != 0:
+            print("set ExposureAuto mode fail! ret[0x%x]" % ret)
+            sys.exit()
+        else:
+            print("set ExposureAuto mode off")
+
+        # Set ExposureTime
+        ret = self.cam.MV_CC_SetFloatValue("ExposureTime", self.ExposureTime)
+        if ret != 0:
+            print("set ExposureTime fail! ret[0x%x]" % ret)
+            sys.exit()
+        else:
+            print("set ExposureTime ExposureTime",self.ExposureTime)
+
+        # Set Gain
+        ret = self.cam.MV_CC_SetFloatValue("Gain", self.Gain)
+        if ret != 0:
+            print("set Gain fail! ret[0x%x]" % ret)
+            sys.exit()
+        else:
+            print("set Gain ExposureTime", self.Gain)
+        # Start grabbing
+        ret = self.cam.MV_CC_StartGrabbing()
+        if ret != 0:
+            print("start grabbing fail! ret[0x%x]" % ret)
+            sys.exit()
+        else:
+            print("start grabbing ")
+
+    def _get_one_frame(self):
+        stOutFrame = MV_FRAME_OUT()  # переменная выходного фрейм  тип данных
+        memset(byref(stOutFrame), 0, sizeof(stOutFrame))  # заполняем всю структуру нулями
+        ret = self.cam.MV_CC_GetImageBuffer(stOutFrame, 10000)  # читаем из буфера камеры
+        img_buff = None
+        if None != stOutFrame.pBufAddr and 0 == ret:
+            print("MV_CC_GetImageBuffer: Width[%d], Height[%d], nFrameNum[%d]" % (stOutFrame.stFrameInfo.nWidth, stOutFrame.stFrameInfo.nHeight, stOutFrame.stFrameInfo.nFrameNum))
+            stConvertParam = MV_CC_PIXEL_CONVERT_PARAM()
+            memset(byref(stConvertParam), 0, sizeof(stConvertParam))
+            stConvertParam.enDstPixelType = PixelType_Gvsp_BGR8_Packed  # opecv要用BGR，不能使用RGB
+            nConvertSize = stOutFrame.stFrameInfo.nWidth * stOutFrame.stFrameInfo.nHeight * 3  # размер цветного кадра
+            # convert pixel
+            if img_buff is None:
+                img_buff = (c_ubyte * stOutFrame.stFrameInfo.nFrameLen)()
+            stConvertParam.nWidth = stOutFrame.stFrameInfo.nWidth
+            stConvertParam.nHeight = stOutFrame.stFrameInfo.nHeight
+            stConvertParam.pSrcData = cast(stOutFrame.pBufAddr, POINTER(c_ubyte))
+            stConvertParam.nSrcDataLen = stOutFrame.stFrameInfo.nFrameLen
+            stConvertParam.enSrcPixelType = stOutFrame.stFrameInfo.enPixelType
+            stConvertParam.pDstBuffer = (c_ubyte * nConvertSize)()
+            stConvertParam.nDstBufferSize = nConvertSize
+            ret = self.cam.MV_CC_ConvertPixelType(stConvertParam)  # конвертируем пиксели в правильном порядке
+            if ret != 0:
+                print("convert pixel fail! ret[0x%x]" % ret)
+                del stConvertParam.pSrcData
+                sys.exit()
+            img_buff = (c_ubyte * stConvertParam.nDstLen)()
+            cdll.msvcrt.memcpy(byref(img_buff), stConvertParam.pDstBuffer, stConvertParam.nDstLen)  # копирование данных
+            img_buff = np.frombuffer(img_buff, count=int(stConvertParam.nDstBufferSize),  # преобразование в np массив
+                                     dtype=np.uint8)  # data以流的形式读入转化成ndarray对象
+            img_buff = img_buff.reshape(stOutFrame.stFrameInfo.nHeight, stOutFrame.stFrameInfo.nWidth, 3)
+            nRet = self.cam.MV_CC_FreeImageBuffer(stOutFrame)
+            if ret != 0:
+                print("MV_CC_FreeImageBuffer fail! ret[0x%x]" % ret)
+                del stConvertParam.pSrcData
+                sys.exit()
+            return img_buff
+        else:
+            imageError = cv2.imread('Test_img.jpg')
+            print("no data[0x%x]" % ret)
+            return imageError
+
+
+class cnnYolo():
+    def __init__(self):
+        self.model = 0
+        self.modelEnginePath ='EMG_2025_24_06_v1.engine'
+
+    def _createModel(self):
+        self.model = YOLO(self.modelEnginePath)
+
+    def objectDetection(self,image):
+        start_time = time.time()
+        img_color_rbb = image
+        heightImg, widthImg, channelsImg = img_color_rbb.shape
+        bytes_per_lineImg = channelsImg * widthImg
+
+        results = self.model(img_color_rbb)
+        annotated_frame = results[0].plot()
+        annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+
+        end_time = time.time()
+        execution_time = end_time - start_time
+        fps = 1 / execution_time
+        print(f"Время выполнения: {execution_time:.6f} секунд, FPS: {fps:.6f}")
+
+        cv2.putText(annotated_frame, f"FPS: {fps:.2f}", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        q_image = QImage(annotated_frame.data, widthImg, heightImg, bytes_per_lineImg, QImage.Format_RGB888)
+        q_pixmap = QPixmap.fromImage(q_image)
+        q_pixmap2 = q_pixmap.copy()
+        return q_pixmap2
 
 
 def _update_cam_list(cam_link,cam_list_link):
@@ -323,10 +459,6 @@ if __name__ == "__main__":
     print(f"GPU device: {torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'None'}")
     print(f"Версия pymodbus: {pymodbus.__version__}")
 
-
-
-
-
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -347,15 +479,17 @@ if __name__ == "__main__":
     #Qt app create
     app = QApplication(sys.argv)
 
-
     hikCamera1 = HikCam(10)
     hikCamera1._update_cam_list()
-    hikCamera1._create_cam_handle_open_setting()
+    hikCamera1._create_cam_handle_open_setting_start_grab()
+
+    cnn1 = cnnYolo()
+    cnn1._createModel()
 
     #client = _modbus_connect()
     #_modbus_read(client)
 
-    model = YOLO('EMG_2025_24_06_v1.engine')
+    # model = YOLO('EMG_2025_24_06_v1.engine')
     ExposureTime = 2000
     Gain = 1.0
 
@@ -384,6 +518,13 @@ if __name__ == "__main__":
     ui.exposureTime_spinBox.setRange(0,20000)
     ui.exposureTime_spinBox.setValue(ExposureTime)
     ui.exposureTime_spinBox.valueChanged.connect(_changeValueExposureTime)
+
+    testLable = hikCamera1._get_one_frame()
+    testPixMape = cnn1.objectDetection(testLable)
+
+    ui.label.setPixmap(testPixMape)
+
+
 
     #imer = QTimer()
     #timer.setInterval(10)
